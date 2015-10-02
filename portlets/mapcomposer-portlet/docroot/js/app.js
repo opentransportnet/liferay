8263871325fab202c8508362be2aac3f81ce513d
Přemysl Vohnout <vohnout@ccss.cz>
2015-10-02 13:19:23 +0200

    mapcomposer initial commit

---
+++ portlets/mapcomposer-portlet/docroot/js/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+define(['angular', 'ol', 'toolbar', 'layermanager', 'map', 'query', 'search', 'print', 'permalink', 'measure', 'legend', 'geolocation', 'core', 'api', 'angular-gettext', 'translations', 'compositions', 'status_creator'],
+
+    function(angular, ol, toolbar, layermanager) {
+        var module = angular.module('hs', [
+            'hs.toolbar',
+            'hs.layermanager',
+            'hs.map',
+            'hs.query',
+            'hs.search', 'hs.print', 'hs.permalink', 'hs.measure',
+            'hs.legend', 'hs.geolocation', 'hs.core',
+            'hs.api',
+            'gettext',
+            'hs.compositions', 'hs.status_creator'
+        ]);
+
+        module.directive('hs', ['hs.map.service', 'Core', function(OlMap, Core) {
+            return {
+                templateUrl: hsl_path + 'hslayers.html',
+                link: function(scope, element) {
+                        var w = angular.element($(window));
+                        w.bind('resize', function() {
+                            var el = $(element[0]);
+                            var windowHeight = $(window).height();
+                            var headerHeight = $('.portlet-dockbar').outerHeight() + $('.breadcrumbs').outerHeight() + $('#header').outerHeight() + 10;
+                            var elementHeight = windowHeight - headerHeight;
+                            el.height(elementHeight);
+                            $("#map").height(elementHeight);
+                            OlMap.map.updateSize();
+                        });
+                        w.resize();
+                }
+            };
+        }]);
+
+        module.value('box_layers', []);
+        module.value('catalogue_url', caturl);
+
+        module.value('default_layers', [
+            new ol.layer.Tile({
+                source: new ol.source.OSM({
+                    wrapX: false
+                }),
+                title: "Base layer",
+                base: true
+            })
+        ]);
+
+        module.value('default_view', new ol.View({
+            center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+            zoom: 5,
+            units: "m"
+        }));
+
+        module.value('project_name', 'myproject');
+
+        module.controller('Main', ['$scope', 'Core', 'hs.query.service_infopanel', 'hs.compositions.service_parser',
+            function($scope, Core, InfoPanelService, composition_parser) {
+                if (console) console.log("Main called");
+                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+                $scope.Core = Core;
+                Core.setMainPanel('composition_browser');
+                //composition_parser.load('http://www.whatstheplan.eu/wwwlibs/statusmanager2/index.php?request=load&id=972cd7d1-e057-417b-96a7-e6bf85472b1e');
+                $scope.$on('infopanel.updated', function(event) {
+                    if (console) console.log('Attributes', InfoPanelService.attributes, 'Groups', InfoPanelService.groups);
+                });
+            }
+        ]);
+
+        return module;
+    });
