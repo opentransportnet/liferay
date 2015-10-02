9eeb558a19cadc8303e4b4c62779ce3b8d58c0f3
Přemysl Vohnout <vohnout@ccss.cz>
2015-10-02 15:03:23 +0200

    trafic density portlet

---
+++ portlets/trafficdensity-portlet/docroot/js/app.js
@@ -0,0 +1,104 @@
+'use strict';
+
+define(['ol',
+        'toolbar',
+        'layermanager',
+        'WfsSource', 'query',
+        'search',
+        'print',
+        'permalink',
+        'measure',
+        'geolocation',
+        'api',
+        'glutils', 'WGL', 'wglinit', 'manager', 'mapcontroller', 'dataloader', 'd3', 'dimension',
+        'heatmapdimension', 'chart_panel', 'stackedbarchart', 'histogramdimension', 'mapdimension', 'floatreaderhistogram',
+        'floatrasterreader', 'linearfilter', 'filter', 'multibrush'
+    ],
+
+    function(ol, toolbar, layermanager, WfsSource) {
+        var module = angular.module('hs', [
+            'hs.toolbar',
+            'hs.layermanager',
+            'hs.query',
+            'hs.search', 'hs.print', 'hs.permalink',
+            'hs.geolocation', 'hs.widgets.chart_panel'
+        ]);
+
+        module.directive('hs', ['hs.map.service', 'Core', '$compile', 'webgl_viz', function(OlMap, Core, $compile, webgl_viz) {
+            return {
+                templateUrl: hsl_path + 'hslayers.html',
+                link: function(scope, element) {
+                    var w = angular.element($(window));
+                    w.bind('resize', function() {
+                        var el = $(element[0]);
+                        var windowHeight = $(window).height();
+                        var headerHeight = $('.portlet-dockbar').outerHeight() + $('.breadcrumbs').outerHeight() + $('#header').outerHeight() + 10;
+                        var elementHeight = windowHeight - headerHeight;
+                        el.height(elementHeight);
+                        $("#map").height(elementHeight);
+                        OlMap.map.updateSize();
+                    });
+                    w.resize();
+                    $("#right-pane", element).append($compile('<div chartpanel ng-controller="ChartPanel"></div>')(scope));
+                    $("#right-pane", element).css({
+                        'width': '560px',
+                        'padding-top': "40px",
+                        'padding-left': '33px'
+                    });
+                    $(".search-container").removeClass('col-md-4').addClass('col-md-3');
+                    //webgl_viz.webgl_el = $compile('<canvas id="webglayer"></canvas>')(scope);
+                    //element.append(webgl_viz.webgl_el);
+                    webgl_viz.init();
+                }
+            };
+        }]);
+
+        module.service('webgl_viz', ['hs.map.service', function(OlMap) {
+            OlMap.map.removeInteraction(OlMap.interactions.DragPan);
+            OlMap.interactions.DragPan = new ol.interaction.DragPan({
+                kinetic: false
+            });
+            OlMap.map.addInteraction(OlMap.interactions.DragPan);
+
+            var me = {
+                map: OlMap.map,
+                ol: ol,
+                init: function() {
+                    wglinit(this);
+                }
+            };
+
+            return me;
+        }]);
+
+        module.value('config', {
+            default_layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM(),
+                    title: "Base layer",
+                    box_id: 'osm',
+                    base: true
+                })
+            ],
+            default_view: new ol.View({
+                center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+                zoom: 4,
+                units: "m"
+            })
+        });
+
+        module.controller('Main', ['$scope', 'Core', 'hs.map.service', 'webgl_viz',
+            function($scope, Core, OlMap, webgl_viz) {
+                if (console) console.log("Main called");
+                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+                $scope.Core = Core;
+
+                var map = OlMap.map;
+                $scope.$on('infopanel.updated', function(event) {});
+
+
+            }
+        ]);
+
+        return module;
+    });
---
+++ portlets/trafficdensity-portlet/docroot/js/config-ng.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var hsl_path = '/wwwlibs/hslayers-ng/';
+var jans_path = 'http://home.zcu.cz/~jezekjan/webglayer-gl-filter2/js/'; //http://localhost:9999/js/webglayer/js/
+
+//var jans_path = 'http://localhost:9999/js/webglayer/js/';
+
+//https://github.com/tnajdek/angular-requirejs-seed
+require.config({
+    paths: {
+        toolbar: hsl_path + 'components/toolbar/toolbar',
+        layermanager: hsl_path + 'components/layermanager/layermanager',
+        map: hsl_path + 'components/map/map',
+        ows: hsl_path + 'components/ows/ows',
+        'ows.wms': hsl_path + 'components/ows/ows_wms',
+        'ows.nonwms': hsl_path + 'components/ows/ows_nonwms',
+        'ows.wmsprioritized': hsl_path + 'components/ows/ows_wmsprioritized',
+        query: hsl_path + 'components/query/query',
+        search: hsl_path + 'components/search/search',
+        print: hsl_path + 'components/print/print',
+        permalink: hsl_path + 'components/permalink/permalink',
+        lodexplorer: hsl_path + 'components/lodexplorer/lodexplorer',
+        geolocation: hsl_path + 'components/geolocation/geolocation',
+        measure: hsl_path + 'components/measure/measure',
+        legend: hsl_path + 'components/legend/legend',
+        app: '/trafficdensity-portlet/js/app',
+        xml2json: hsl_path + 'lib/xml2json.min',
+        panoramio: hsl_path + 'components/panoramio/panoramio',
+        core: hsl_path + 'components/core/core',
+        WfsSource: hsl_path + 'extensions/hs.source.Wfs',
+        api: hsl_path + 'components/api/api',
+        translations: hsl_path + 'components/translations/js/translations',
+        dimension: jans_path + '/Dimension',
+        glutils: jans_path + 'GLUtils',
+        manager: jans_path + 'Manager',
+        mapcontroller: jans_path + 'MapController',
+        heatmapdimension: jans_path + 'HeatMapDimension',
+        mapdimension: jans_path + 'MapDimension',
+        stackedbarchart: jans_path + 'StackedBarChart',
+        histogramdimension: jans_path + 'HistDimension',
+        floatrasterreader: jans_path + 'FloatRasterReader',
+        floatreaderhistogram: jans_path + 'FloatReaderHistogram',
+        linearfilter: jans_path + 'LinearFilter',
+        multibrush: jans_path + 'd3.svg.multibrush',
+        WGL: jans_path + 'WGL',
+        filter: jans_path + 'Filter',
+        dataloader: '/trafficdensity-portlet/js/DataLoader',
+        ol: hsl_path + 'lib/ol3/ol-full',
+        wglinit: '/trafficdensity-portlet/js/webglinit',
+        chart_panel: hsl_path + 'examples/webgl_viz/chart_panel/chart_panel',
+
+
+    },
+    shim: {
+        d3: {
+            exports: 'd3'
+        },
+        multibrush: {
+            deps: ['d3']
+        }
+    }
+
+});
+
+//http://code.angularjs.org/1.2.1/docs/guide/bootstrap#overview_deferred-bootstrap
+window.name = "NG_DEFER_BOOTSTRAP!";
+
+require(['core'], function(app) {
+
+    require(['app'], function(app) {
+        var $html = angular.element(document.getElementsByTagName('html')[0]);
+        angular.element().ready(function() {
+            angular.resumeBootstrap([app['name']]);
+        });
+    });
+});
