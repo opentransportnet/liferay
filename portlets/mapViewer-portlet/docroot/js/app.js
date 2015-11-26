e5ba4482ebef293d8077495b79b4d1f53fae646f
Přemysl Vohnout <vohnout@ccss.cz>
2015-11-26 17:07:38 +0100

    mapviewer update

--- portlets/mapViewer-portlet/docroot/js/app.js
+++ portlets/mapViewer-portlet/docroot/js/app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-define(['ol', 'core', 'api', 'toolbar', 'layermanager', 'map', 'query', 'search', 'permalink', 'measure', 'datasource_selector', 'ows', 'WfsSource', 'angular-gettext', 'translations', 'compositions', 'status_creator'],
+define(['angular', 'ol', 'core', 'api', 'sidebar', 'toolbar', 'layermanager', 'map', 'query', 'search', 'permalink', 'measure', 'datasource_selector', 'ows', 'WfsSource', 'angular-gettext', 'translations', 'compositions', 'status_creator', 'customhtml'],
 
-    function(ol, toolbar, layermanager, WfsSource) {
+    function(angular, ol, toolbar, layermanager, WfsSource) {
         var module = angular.module('hs', [
+            'hs.sidebar',
             'hs.toolbar',
             'hs.layermanager',
             'hs.map',
@@ -15,24 +16,28 @@ define(['ol', 'core', 'api', 'toolbar', 'layermanager', 'map', 'query', 'search'
             'hs.api',
             'hs.ows',
             'gettext',
-            'hs.compositions'
+            'hs.compositions',
+            'hs.customhtml'
         ]);
 
         module.directive('hs', ['hs.map.service', 'Core', function(OlMap, Core) {
             return {
-                templateUrl: hsl_path + 'hslayers.html',
+                templateUrl: hsl_path + '/hslayers.html',
                 link: function(scope, element) {
-                        var w = angular.element($(window));
-                        w.bind('resize', function() {
-                            var el = $(element[0]);
-                            var windowHeight = $(window).height();
-                            var headerHeight = $('.portlet-dockbar').outerHeight();
-                            var elementHeight = windowHeight - headerHeight;
-                            el.height(elementHeight);
-                            $("#map").height(elementHeight);
-                            OlMap.map.updateSize();
-                        });
-                        w.resize();
+                    var w = angular.element($(window));
+                    w.bind('resize', function() {
+                        $("html").css('overflow', 'hidden');
+                        var el = $(element[0]);
+                        var windowHeight = $(window).height();
+                        var headerHeight = $('.portlet-dockbar').outerHeight();
+                        var elementHeight = windowHeight - headerHeight;
+                        el.height(elementHeight);
+                        $("#map").height(elementHeight);
+                        $("#panelplace").height(elementHeight);
+                        Core.updateMapSize();
+                        OlMap.map.updateSize();
+                    });
+                    w.resize();
                 }
             };
         }]);
@@ -54,8 +59,30 @@ define(['ol', 'core', 'api', 'toolbar', 'layermanager', 'map', 'query', 'search'
                 zoom: 5,
                 units: "m"
             }),
+            datasources: [{
+                   title: "CKAN",
+                   url: "/otnServices-1.0/platform/ckanservices/datasets",
+                   language: 'eng',
+                   type: "ckan"
+               }, {
+                   title: "Micka",
+                   url: "/php/metadata/csw/",
+                   language: 'eng',
+                   type: "micka",
+                   code_list_url: '/php/metadata/util/codelists.php?_dc=1440156028103&language=eng&page=1&start=0&limit=25&filter=%5B%7B%22property%22%3A%22label%22%7D%5D'
+               }
+            ],
             'catalogue_url': caturl,
-            'compositions_catalogue_url': caturl
+            'compositions_catalogue_url': caturl,
+            status_manager_url: '/wwwlibs/statusmanager2/index.php',
+            createExtraMenu: function($compile, $scope, element) {
+                $scope.uploadClicked = function() {
+                    alert("UPLOAD!")
+                }
+                var el = angular.element("<li class=\"sidebar-item\" ng-click=\"uploadClicked()\" ><a href=\"#\"><span class=\"menu-icon fa fa-cloud-upload\"></span><span class=\"sidebar-item-title\">Upload</span></a></li>");
+                element.find('ul').append(el);
+                $compile(el)($scope);
+            }
         });
 
         module.controller('Main', ['$scope', 'Core', 'hs.query.service_infopanel', 'hs.compositions.service_parser',
@@ -64,6 +91,7 @@ define(['ol', 'core', 'api', 'toolbar', 'layermanager', 'map', 'query', 'search'
                 $scope.hsl_path = hsl_path; //Get this from hslayers.js file
                 $scope.Core = Core;
                 Core.setMainPanel('composition_browser');
+                Core.sidebarRight=false;
                 $scope.$on('infopanel.updated', function(event) {
                     if (console) console.log('Attributes', InfoPanelService.attributes, 'Groups', InfoPanelService.groups);
                 });
--- portlets/mapViewer-portlet/docroot/js/config-ng.js
+++ portlets/mapViewer-portlet/docroot/js/config-ng.js
@@ -3,24 +3,22 @@
 var hsl_path = '/wwwlibs/hslayers-ng/';
 require.config({
     paths: {
+        sidebar: hsl_path + 'components/sidebar/sidebar',
         toolbar: hsl_path + 'components/toolbar/toolbar',
         layermanager: hsl_path + 'components/layermanager/layermanager',
         ows: hsl_path + 'components/ows/ows',
-        ol: hsl_path + 'lib/ol3/ol-full',
         'ows.wms': hsl_path + 'components/ows/ows_wms',
         'ows.nonwms': hsl_path + 'components/ows/ows_nonwms',
         'ows.wmsprioritized': hsl_path + 'components/ows/ows_wmsprioritized',
         query: hsl_path + 'components/query/query',
         search: hsl_path + 'components/search/search',
-        print: hsl_path + 'components/print/print',
         permalink: hsl_path + 'components/permalink/permalink',
-        lodexplorer: hsl_path + 'components/lodexplorer/lodexplorer',
         geolocation: hsl_path + 'components/geolocation/geolocation',
         measure: hsl_path + 'components/measure/measure',
         legend: hsl_path + 'components/legend/legend',
         app: '/mapViewer-portlet/js/app',
         core: hsl_path + 'components/core/core',
-        WfsSource: hsl_path + 'extensions/hs.source.Wfs',
+        WfsSource: hsl_path + 'components/layers/hs.source.Wfs',
         datasource_selector: hsl_path + 'components/datasource_selector/datasource_selector',
         api: hsl_path + 'components/api/api',
         translations: hsl_path + 'components/translations/js/translations'
