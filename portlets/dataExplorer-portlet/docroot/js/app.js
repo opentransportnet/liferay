5beb4b461c226ce79d6c8fd9591b421b136afd2c
Přemysl Vohnout <premek@vohnout.cz>
2015-11-27 18:40:04 +0100

    new portlets

---
+++ portlets/dataExplorer-portlet/docroot/js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+define(['angular', 'ol', 'core', 'api', 'layermanager', 'sidebar', 'toolbar', 'map', 'query', 'search', 'permalink', 'datasource_selector', 'angular-gettext', 'translations'],
+
+    function(angular, ol, toolbar, layermanager) {
+        var module = angular.module('hs', [
+            'hs.sidebar',
+            'hs.toolbar',
+            'hs.map',
+            'hs.layermanager',
+            'hs.query',
+            'hs.search', 'hs.permalink',
+            'hs.core',
+            'hs.datasource_selector',
+            'hs.api',
+            'gettext',
+        ]);
+
+        module.directive('hs', ['hs.map.service', 'Core', function(OlMap, Core) {
+            return {
+                templateUrl: hsl_path + '/hslayers.html',
+                link: function(scope, element) {
+                    var w = angular.element($(window));
+                    w.bind('resize', function() {
+                        $("html").css('overflow', 'hidden');
+                        var el = $(element[0]);
+                        var windowHeight = $(window).height();
+                        var headerHeight = $('.portlet-dockbar').outerHeight();
+                        var elementHeight = windowHeight - headerHeight;
+                        el.height(elementHeight);
+                        $("#map").height(elementHeight);
+                        $("#panelplace").height(elementHeight);
+                        Core.updateMapSize();
+                        OlMap.map.updateSize();
+                    });
+                    w.resize();
+                }
+            };
+        }]);
+
+        module.value('config', {
+            default_layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM({
+                        wrapX: false
+                    }),
+                    title: "Base layer",
+                    base: true,
+                    removable: false
+                })
+            ],
+            project_name: 'otn/map',
+            default_view: new ol.View({
+                center: ol.proj.transform([17.474129, 52.574000], 'EPSG:4326', 'EPSG:3857'), //Latitude longitude    to Spherical Mercator
+                zoom: 5,
+                units: "m"
+            }),
+            datasources: [{
+                   title: "Data",
+                   url: "/otnServices-1.0/platform/ckanservices/datasets",
+                   language: 'eng',
+                   type: "ckan"
+               }, {
+                   title: "Metadata",
+                   url: "/php/metadata/csw/",
+                   language: 'eng',
+                   type: "micka",
+                   code_list_url: '/php/metadata/util/codelists.php?_dc=1440156028103&language=eng&page=1&start=0&limit=25&filter=%5B%7B%22property%22%3A%22label%22%7D%5D'
+               }
+            ],
+            'catalogue_url': caturl,
+            'compositions_catalogue_url': caturl,
+            status_manager_url: '/wwwlibs/statusmanager2/index.php',
+        });
+
+        module.controller('Main', ['$scope', 'Core', 'hs.query.service_infopanel',
+            function($scope, Core, InfoPanelService) {
+                if (console) console.log("Main called");
+                $scope.hsl_path = hsl_path; //Get this from hslayers.js file
+                $scope.Core = Core;
+                Core.setDefaultPanel('datasource_selector');
+                Core.sidebarButtons=false;
+                Core.sidebarRight=false;
+                $scope.$on('infopanel.updated', function(event) {
+                    if (console) console.log('Attributes', InfoPanelService.attributes, 'Groups', InfoPanelService.groups);
+                });
+            }
+        ]);
+
+        return module;
+    });
---
+++ portlets/mapCreator-portlet/docroot/html/view.jsp
@@ -0,0 +1,6 @@
+<%@include file="/html/init.jsp"%>
+
+
+    <div hs ng-app="hs" ng-controller="Main" style="position: relative;"></div>
+
+<script>var caturl = "<%=caturl%>";</script>
